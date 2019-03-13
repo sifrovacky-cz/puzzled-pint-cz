dfc4c0ba8488356fc53297c6564cc07acb2bcea1
Ales Podolnik <podolnik@ipp.cas.cz>
2019-03-13 09:57:08 +0100

    April 2019

--- puzzled-pint-cz.js
+++ puzzled-pint-cz.js
@@ -1,15 +1,19 @@
 	function switchLanguage(lang) {
 		if (lang=="cs") {
-			document.getElementById("texten").className = "text hidden";
+            document.getElementById("texten").className = "text hidden";
+            document.getElementById("linken").className = "text hidden";
 			document.getElementById("menuen").className = "hidden";
-			document.getElementById("textcs").className = "text";
+            document.getElementById("textcs").className = "text";
+            document.getElementById("linkcs").className = "text";
 			document.getElementById("menucs").className = "";
 
 		}
 		else {
-			document.getElementById("textcs").className = "text hidden";
+            document.getElementById("textcs").className = "text hidden";
+            document.getElementById("linkcs").className = "text hidden";
 			document.getElementById("menucs").className = "hidden";
-			document.getElementById("texten").className = "text";
+            document.getElementById("texten").className = "text";
+            document.getElementById("linken").className = "text";
 			document.getElementById("menuen").className = "";
 		};
 	};
